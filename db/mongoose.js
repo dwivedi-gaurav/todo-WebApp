eaba92df5fd27cca57f90afcd9fb797dee16980e
Gaurav Dwivedi <dwivedig@HCL.COM>
2019-04-30 11:57:03 +0530

    Update and Delete todos

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose=require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/TodoWebApp',{useNewUrlParser: true,useCreateIndex: true}).then(()=>{
+mongoose.connect('mongodb://localhost:27017/TodoWebApp',{useNewUrlParser: true,useCreateIndex: true,useFindAndModify: false}).then(()=>{
   console.log('Connected to MongoDB');
 }).catch((e)=>{
   console.log(e);
--- routes/todos.js
+++ routes/todos.js
@@ -2,10 +2,19 @@ var express=require('express');
 var router=express.Router();
 var {Todo}=require('../models/Todo');
 var {ensureAuthenticated}=require('../middleware/auth');
+var {ObjectID}=require('mongodb');
 
 //Todos home page
 router.get('/home',ensureAuthenticated,(req,res)=>{
-  res.render('home');
+  var p1=Todo.find({completed:false});
+  var p2=Todo.find({completed:true});
+
+  Promise.all([p1,p2]).then((todos)=>{
+    res.render('home',{todos,ObjectID});
+  }).catch((e)=>{
+    req.flash('error_msg','Could not load todos, because of some database error.');
+    res.redirect('/todos/home');
+  });
 });
 
 //Add todo
@@ -14,7 +23,7 @@ router.post('/add',ensureAuthenticated,(req,res)=>{
   var errors=[];
   if(todo==''){
     errors.push({msg:'Enter a Todo.'});
-    res.render('home',{errors,todo});
+    res.render('home',{errors});
   }else{
     var todo =new Todo({todo,_creator:req.user.id});
     todo.save({todo}).then((todo)=>{
@@ -28,5 +37,44 @@ router.post('/add',ensureAuthenticated,(req,res)=>{
   }
 });
 
+router.post('/update',(req,res)=>{
+  var {id,todo,done}=req.body;
+  body={todo};
+  if(done){
+    body.completed=true;
+    body.completedAt=new Date().getTime();
+  }else{
+    body.completed=false;
+    body.completedAt=null;
+  }
+  Todo.findOneAndUpdate({
+    _id:id,
+    _creator:req.user.id
+  },body,{new:true}).then((todo)=>{
+    req.flash('success_msg','Todo updated successfully.');
+    res.redirect('/todos/home');
+  }).catch((e)=>{
+    req.flash('error_msg','Database error.');
+    res.redirect('/todos/home');
+  });
+});
+
+router.get('/delete/:id',(req,res)=>{
+  Todo.findOneAndDelete({
+    _id:req.params.id,
+    _creator:req.user.id
+  }).then((todo)=>{
+    if(!todo){
+      req.flash('error_msg','Reuested todo not found.');
+      res.redirect('/todos/home');
+    }else{
+      req.flash('success_msg','Todo deleted successfully.');
+      res.redirect('/todos/home');
+    }
+  }).catch((e)=>{
+    req.flash('error_msg','Database error');
+    res.redirect('/todos/home');
+  });
+});
 
 module.exports=router;
